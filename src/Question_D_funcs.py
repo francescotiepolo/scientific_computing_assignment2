601a02d17e0c2d41b0f7dd3bb798d453f3dc088d
Ilias Sofianos <elias_01@hotmail.gr>
2025-03-03 14:45:22 +0100

    added numba jit

--- src/Question_D_funcs.py
+++ src/Question_D_funcs.py
@@ -1,45 +1,53 @@
 import numpy as np
+from numba import jit
 
+@jit(nopython=True)
 def initialize_grid(size, noise=0.02):
     u = np.ones((size, size)) * 0.5
     v = np.zeros((size, size))
-    
-    #small square in the center with v = 0.25
-    r = size // 10  #region size
+
+    # Small square in the center with v = 0.25
+    r = size // 10  # Region size
     cx, cy = size // 2, size // 2
     v[cx - r:cx + r, cy - r:cy + r] = 0.25
-    
-    #add some noise
+
+    # Add some noise
     u += noise * np.random.rand(size, size)
     v += noise * np.random.rand(size, size)
-    
+
     return u, v
 
+@jit(nopython=True)
 def laplacian(Z):
-    return (
-        np.roll(Z, 1, axis=0) + np.roll(Z, -1, axis=0) +
-        np.roll(Z, 1, axis=1) + np.roll(Z, -1, axis=1) -
-        4 * Z
-    )
+    Z_up = np.roll(Z, 1, axis=0)
+    Z_down = np.roll(Z, -1, axis=0)
+    Z_left = np.roll(Z, 1, axis=1)
+    Z_right = np.roll(Z, -1, axis=1)
 
+    return (Z_up + Z_down + Z_left + Z_right - 4 * Z)
+
+@jit(nopython=True)
 def update(u, v, Du, Dv, f, k, dt, dx, noise=0.0):
     Lu = laplacian(u) / dx**2
     Lv = laplacian(v) / dx**2
-    
+
     uvv = u * v**2
     u += (Du * Lu - uvv + f * (1 - u)) * dt + noise * np.random.rand(*u.shape)
     v += (Dv * Lv + uvv - (f + k) * v) * dt + noise * np.random.rand(*v.shape)
-    
+
     return u, v
 
+@jit(nopython=True)
 def run_simulation(size, steps, parameter_sets, dx, dt, noise=0.0):
     results = []
-    for Du, Dv, f, k in parameter_sets:
+    for i in range(len(parameter_sets)):
+        Du, Dv, f, k = parameter_sets[i]
         u, v = initialize_grid(size, noise=noise)
         for _ in range(steps):
             u, v = update(u, v, Du, Dv, f, k, dt, dx, noise=noise)
         results.append((u, v, Du, Dv, f, k, noise))
     return results
+
 
 
 
